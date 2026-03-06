c59405b412f80020486a3c2b865fe23344b5e8e1
SantiagoBonillaGuevara <115745269+SantiagoBonillaGuevara@users.noreply.github.com>
2026-03-06 02:34:52 +0100

    Implement button to toggle ADC readings
    Added button functionality to enable/disable ADC readings.

--- ADC-DAC/adc.ino
+++ ADC-DAC/adc.ino
@@ -1,16 +1,37 @@
 #include <Arduino.h>
 
 const int adcPin = 34;
+const int buttonPin = 18;
+
+bool adcEnabled = false;
+bool lastButtonState = HIGH;
+
 
 void setup() {
   Serial.begin(115200);
+  pinMode(buttonPin, INPUT_PULLUP);
 }
 
 void loop() {
 
-  int value = analogRead(adcPin);
-  
-  Serial.print(">value:");
-  Serial.println(value);
+  bool buttonState = digitalRead(buttonPin) == LOW;
+
+  if(buttonState == LOW && lastButtonState == HIGH){
+    adcEnabled = !adcEnabled;   // cambiar estado
+    delay(200); // debounce simple
+  }
+
+  lastButtonState = buttonState;
+
+  if(adcEnabled){
+    int value = analogRead(adcPin);
+    Serial.print(">value:");
+    Serial.println(value);
+    Serial.print("habilitado, valor: ");
+    Serial.println(value);
+  }
+  else{
+    Serial.println("ADC deshabilitado");
+  }
   delay(100);
 }
